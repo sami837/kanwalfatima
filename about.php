15469cc7369efc69904b8aed9f81e56da62cc58d
kanwal <kanwal562@gmail.com>
2021-04-18 11:41:42 +0500

    added

---
+++ about.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Admin Panel</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+		
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+		<h1>Hello kanwal</h1>
+      hsdfuhtuh
+  </body>
+</html>
